d0727cfd1737520c748f5e084bdacb9c8e519f37
forrest <fspmarshall@gmail.com>
2016-12-01 13:32:56 -1000

    therm simplified

--- src/spi/thermristor.py
+++ src/spi/thermristor.py
@@ -23,13 +23,23 @@ def nlread(spi,ch,avg):
     reading += spi.read(ch)
     return reading / avg
 
+# Get a reading.
+def read_val(spi,ch,avg,lock):
+    reading = 0.0
+    for i in range(avg-1):
+        reading += spi.read(ch,lock=lock)
+        time.sleep(0.3)
+    reading += spi.read(ch,lock=lock)
+    return reading / avg
+
 # Generate a log/reading at a specific time.
 def mklog(spi,ch,t,avg,lock):
     delta = t - time.time()
     reading = 0.0
     if delta > 0.1: time.sleep(delta)
-    if lock: return lkread(spi,ch,avg,lock)
-    else: return nlread(spi,ch,avg)
+    return read_val(spi,ch,avg,lock)
+    #if lock: return lkread(spi,ch,avg,lock)
+    #else: return nlread(spi,ch,avg)
 
 # Master function -- orchestrates logging.
 # Args: channel num, uxtime schedule, [average num], [threading lock]
